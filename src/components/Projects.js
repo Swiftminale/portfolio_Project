e574e407c955b6bdbd40d7bd9003b3cb55cff839
Swiftminale <minalefetene@gmail.com>
2024-06-04 23:35:55 +0300

    changed ProjectTabs in projects, ProjectCard

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -62,13 +62,13 @@ export const Projects = () => {
                       id="pills-tab"
                     >
                       <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        <Nav.Link eventKey="first">Project Template</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        <Nav.Link eventKey="second">Detail</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                        <Nav.Link eventKey="third">Reference</Nav.Link>
                       </Nav.Item>
                     </Nav>
                     <Tab.Content
