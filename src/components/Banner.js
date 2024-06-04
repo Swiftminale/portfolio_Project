95089dc3fa0cd1a3388481113edb9bcf538ab16a
Swiftminale <minalefetene@gmail.com>
2024-06-04 23:47:03 +0300

    modified the Banner to animate the text. [working]

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,16 +5,18 @@ import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
-  const [isDeleted, setIsDeleted] = useState(false);
-  const toRotate = ["Web Developer", "React Developer", "MERN Stack Developer"];
+  const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState("");
-  const period = 2000; // Decreased the period for faster rotation
-  const [delta, setDelta] = useState(300 - Math.random() * 100); // Decreased delta for faster typing
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const period = 2000; // Decreased delta for faster typing
 
   useEffect(() => {
     let ticker = setInterval(() => {
       tick();
     }, delta);
+
     return () => {
       clearInterval(ticker);
     };
@@ -23,23 +25,27 @@ export const Banner = () => {
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleted
+    let updatedText = isDeleting
       ? fullText.substring(0, text.length - 1)
       : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
-    if (isDeleted) {
+    if (isDeleting) {
       setDelta((prevDelta) => prevDelta / 2);
     }
 
-    if (!isDeleted && text === fullText) {
-      setIsDeleted(true);
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleted && updatedText === "") {
-      setIsDeleted(false);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
       setLoopNum(loopNum + 1);
-      setDelta(500); // Adjusted delta for a more consistent typing speed
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
     }
   };
 
