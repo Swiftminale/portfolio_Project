7baf45bb548adc69689fee834f2a6f67cdf21825
Swiftminale <minalefetene@gmail.com>
2024-06-04 23:55:39 +0300

    Added Minale.png and modified Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/Minale.png";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
