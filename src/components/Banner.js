ea7396a3359ce9a34bb6e039cf05305afc931303
Swiftminale <minalefetene@gmail.com>
2024-06-27 15:20:01 +0300

    changed skills In Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ export const Banner = () => {
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const toRotate = ["Web Developer", "Project Manager", "Product Owner"];
   const period = 2000; // Decreased delta for faster typing
 
   useEffect(() => {
