8bebd6480327b489ae74707449de084369d70c45
Swiftminale <minalefetene@gmail.com>
2024-06-27 11:36:43 +0300

    added Speed Insights

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+
+      <SpeedInsights />
     </div>
   );
 }
